be1aa3e6aebe77a5caecb413d2fb8178a71d2795
Georgi Minchev <georgiminchev1206@gmail.com>
2024-12-08 17:46:14 +0200

    Add project files.

---
+++ HanoiTowers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Въведете броя на дисковете:");
+        int discs = int.Parse(Console.ReadLine());
+
+        SolveHanoiTowers(discs, "1", "3", "2");
+    }
+
+    static void SolveHanoiTowers(int n, string startRod, string endRod, string tempRod)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+
+        SolveHanoiTowers(n - 1, startRod, tempRod, endRod);
+
+        Console.WriteLine($"{startRod}-->{endRod}");
+
+        SolveHanoiTowers(n - 1, tempRod, endRod, startRod);
+    }
+}
